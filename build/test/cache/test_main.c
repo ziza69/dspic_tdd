bd5af541e0e4d97a54bae0d514cfdce726f24d8a
gica <gica@local.com>
2016-12-06 10:45:00 +0100

    Init

---
+++ build/test/cache/test_main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "unity.h"
+
+
+void setUp(void)
+
+{
+
+}
+
+
+
+void tearDown(void)
+
+{
+
+}
+
+
+
+void test_main_NeedToImplement(void)
+
+{
+
+    UnityIgnore( (("Need to Implement main")), (UNITY_UINT)(14));
+
+}
